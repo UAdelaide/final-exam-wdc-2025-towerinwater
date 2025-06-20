a194097c700f9b57e84ed12e71eda1b92eca6d69
towerinwater <haitri62@gmail.com>
2025-06-20 03:35:37 +0000

    [automatic commit] 2025-06-20 03:35:37 UTC 129.127.202.120

--- part1/app.js
+++ part1/app.js
@@ -37,8 +37,13 @@ let db;
         });
 
         await db.query(`
-            
-        `)
+            INSERT INTO Users (username, email, password_hash, role) VALUES
+            ('alice123',  'alice@example.com',  'hashed123', 'owner'),
+            ('bobwalker', 'bob@example.com',    'hashed456', 'walker'),
+            ('carol123',  'carol@example.com',  'hashed789', 'owner'),
+            ('hai', 'hai@example.com',   'hashed888', 'owner'),
+            ('phuong', 'phuong@example.com',    'hashed126', 'owner');
+        `);
 
     } catch (err) {
         console.error('Error setting up database. Ensure Mysql is running: service mysql start', err);
