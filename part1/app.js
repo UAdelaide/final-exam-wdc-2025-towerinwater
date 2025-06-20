365a23d01cd6121fe54167a1d5487f288623903c
towerinwater <haitri62@gmail.com>
2025-06-20 03:32:16 +0000

    [automatic commit] 2025-06-20 03:32:16 UTC 129.127.202.120

--- part1/app.js
+++ part1/app.js
@@ -35,6 +35,8 @@ let db;
         password: '',
         database: 'DogWalkService'
         });
+
+
     } catch (err) {
         console.error('Error setting up database. Ensure Mysql is running: service mysql start', err);
     }
