8834c54e49b2f9144ca64d6de7597c8f0fd97e62
towerinwater <haitri62@gmail.com>
2025-06-20 03:15:28 +0000

    [automatic commit] 2025-06-20 03:15:28 UTC 129.127.202.120

--- part1/q.sql
+++ part1/q.sql
@@ -9,7 +9,7 @@
 INSERT INTO Dogs (name, size, owner_id) VALUES
     ('Max', 'medium', (SELECT user_id FROM Users WHERE username = 'alice123')),
     ('Bella', 'small', (SELECT user_id FROM Users WHERE username = 'carol123')),
-    ('Onion', 'medium', (SELECT user_id FROM Users WHERE username = '')),
+    ('Onion', 'medium', (SELECT user_id FROM Users WHERE username = 'alice123')),
     ('Wi', 'large', (SELECT user_id FROM Users WHERE username = 'hai')),
     ('Bo', 'medium', (SELECT user_id FROM Users WHERE username = 'phuong'));
 
