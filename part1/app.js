b7303fddfd3af2d38e42d4b9458402e1704112a5
towerinwater <haitri62@gmail.com>
2025-06-20 03:58:08 +0000

    [automatic commit] 2025-06-20 03:58:08 UTC 129.127.202.120

--- part1/app.js
+++ part1/app.js
@@ -94,7 +94,7 @@ app.get('/api/walkrequests/open', async (req, res) => {
                 u.username AS name
             FROM WalkRequests AS wr
                 INNER JOIN Dogs AS d ON wr.dog_id = d.dog_id, wr.user_id = d.owner_id
-                INNER JOIN Users AS u ON wr.user_id = u.user_id
+                INNER JOIN Users AS u ON d.owner_id = u.user_id
             WHERE wr.status = 'open'
         `);
         res.json(rows);
