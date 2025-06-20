03e71af62a7e8e78ec815bc9f073d8bc58db57d3
towerinwater <haitri62@gmail.com>
2025-06-20 11:38:24 +0000

    [automatic commit] 2025-06-20 11:38:24 UTC 129.127.202.120

--- part2/routes/dog.js
+++ part2/routes/dog.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models/db');
 
-router.get('/dogs', async (req, res) => {
+router.get('/show', async (req, res) => {
     try{
         const [rows] = await db.query(`
             SELECT
