b9e349a3abf950ab6bb97c0870a7606736562bd7
towerinwater <haitri62@gmail.com>
2025-06-20 05:42:13 +0000

    [automatic commit] 2025-06-20 05:42:13 UTC 129.127.202.120

--- part2/app.js
+++ part2/app.js
@@ -8,6 +8,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
+app.use()
 
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
