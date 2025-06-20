928df8248f12d3d1f29cc390973c8abbd308e095
towerinwater <haitri62@gmail.com>
2025-06-20 03:06:04 +0000

    [automatic commit] 2025-06-20 03:06:04 UTC 129.127.202.120

--- part1/q.sql
+++ part1/q.sql
@@ -2,5 +2,5 @@ INSERT INTO Users (username, email, password_hash, role) VALUES
     ('alice123',  'alice@example.com',  'hashed123', 'owner'),
     ('bobwalker', 'bob@example.com',    'hashed456', 'walker'),
     ('carol123',  'carol@example.com',  'hashed789', 'owner'),
-    ('hai', 'hai@example.com',   'hashed012', 'owner'),
-    ('phuong', 'phuong@example.com',    'hashed345', 'walker');
+    ('hai', 'hai@example.com',   'hashed888', 'owner'),
+    ('phuong', 'phuong@example.com',    'hashed126', 'owner');
