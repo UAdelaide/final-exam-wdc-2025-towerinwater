32990ef0b0e470f7878f575b82ebe04b28e3de62
towerinwater <haitri62@gmail.com>
2025-06-20 04:11:13 +0000

    [automatic commit] 2025-06-20 04:11:13 UTC 129.127.202.120

--- part1/app.js
+++ part1/app.js
@@ -35,7 +35,7 @@ let db;
         database: 'DogWalkService'
         });
 
-        await db.query(`
+        /*await db.query(`
             INSERT INTO Users (username, email, password_hash, role) VALUES
                 ('alice123',  'alice@example.com',  'hashed123', 'owner'),
                 ('bobwalker', 'bob@example.com',    'hashed456', 'walker'),
