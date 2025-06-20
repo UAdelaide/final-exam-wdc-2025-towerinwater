425770bd11e5d62cfd1d52ae7b4e01bfd9aa4020
towerinwater <haitri62@gmail.com>
2025-06-20 03:30:35 +0000

    [automatic commit] 2025-06-20 03:30:35 UTC 129.127.202.120

--- part1/app.js
+++ part1/app.js
@@ -16,28 +16,28 @@ app.use(cookieParser());
 let db;
 
 (async () => {
-  try {
-    // Connect to MySQL without specifying a database
-    const connection = await mysql.createConnection({
-      host: 'localhost',
-      user: 'root',
-      password: '' // Set your MySQL root password
-    });
+    try {
+        // Connect to MySQL without specifying a database
+        const connection = await mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '' // Set your MySQL root password
+        });
 
-    // Create the database if it doesn't exist
-    await connection.query('CREATE DATABASE IF NOT EXISTS DogWalkService');
-    await connection.end();
+        // Create the database if it doesn't exist
+        await connection.query('CREATE DATABASE IF NOT EXISTS DogWalkService');
+        await connection.end();
 
-    // Now connect to the created database
-    db = await mysql.createConnection({
-      host: 'localhost',
-      user: 'root',
-      password: '',
-      database: 'DogWalkService'
-    });
-  } catch (err) {
-    console.error('Error setting up database. Ensure Mysql is running: service mysql start', err);
-  }
+        // Now connect to the created database
+        db = await mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'DogWalkService'
+        });
+    } catch (err) {
+        console.error('Error setting up database. Ensure Mysql is running: service mysql start', err);
+    }
 })();
 
 // Route to return books as JSON
