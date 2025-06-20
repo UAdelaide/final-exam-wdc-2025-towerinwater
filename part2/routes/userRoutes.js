95af902a5ecc00a8a33d9a5bd16903b0cb9f9e07
towerinwater <haitri62@gmail.com>
2025-06-20 11:14:32 +0000

    [automatic commit] 2025-06-20 11:14:32 UTC 129.127.202.120

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -32,7 +32,7 @@ router.get('/me', (req, res) => {
 	if (!req.session.user) {
 		return res.status(401).json({ error: 'Not logged in' });
 	}
-	res.status(200).json({ messsage: 'Successfully Loaded Info', info: req.session.user });
+	res.status(200).json({ messsage: 'Successfully Loaded Info', info: req.session.user.user_id });
 });
 
 /* Updated POST of login */
