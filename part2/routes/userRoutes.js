e21fae1e2b33915389ecfd40920c65d8aaf94a86
towerinwater <haitri62@gmail.com>
2025-06-20 09:22:00 +0000

    [automatic commit] 2025-06-20 09:22:00 UTC 129.127.202.120

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -60,7 +60,11 @@ router.post('/login', async (req, res) => {
 });
 
 router.post('/logout', (req, res) => {
-	req.session.destroy()
+	req.session.destroy((err) => {
+		if(err){
+			return res.status()
+		}
+	});
 });
 
 module.exports = router;
