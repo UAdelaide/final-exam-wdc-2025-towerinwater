18370123204711c7973abde91d3da75271fb3ed5
towerinwater <haitri62@gmail.com>
2025-06-20 02:30:48 +0000

    [automatic commit] 2025-06-20 02:30:48 UTC 129.127.202.120

--- part1/db.js
+++ part1/db.js
@@ -4,7 +4,7 @@ const pool = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'dogwalks.sql'
+    database: 'DogWalkService'
 });
 
 module.exports = pool.promise();
