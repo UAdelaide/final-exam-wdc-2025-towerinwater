228b78b254c789be1d9ac41a3da211ae226cda6f
towerinwater <haitri62@gmail.com>
2025-06-20 03:14:48 +0000

    [automatic commit] 2025-06-20 03:14:48 UTC 129.127.202.120

--- part1/q.sql
+++ part1/q.sql
@@ -9,11 +9,15 @@
 INSERT INTO Dogs (name, size, owner_id) VALUES
     ('Max', 'medium', (SELECT user_id FROM Users WHERE username = 'alice123')),
     ('Bella', 'small', (SELECT user_id FROM Users WHERE username = 'carol123')),
+    ('', 'small', (SELECT user_id FROM Users WHERE username = 'carol123')),
     ('Wi', 'large', (SELECT user_id FROM Users WHERE username = 'hai')),
     ('Bo', 'medium', (SELECT user_id FROM Users WHERE username = 'phuong'));
 
 INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status) VALUES
     ((SELECT dog_id FROM Dogs WHERE name = 'Max'), '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
+    ((SELECT dog_id FROM Dogs WHERE name = 'Bella'), '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted'),
+    ((SELECT dog_id FROM Dogs WHERE name = 'Bella'), '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted'),
+    ((SELECT dog_id FROM Dogs WHERE name = 'Bella'), '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted'),
     ((SELECT dog_id FROM Dogs WHERE name = 'Bella'), '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted'),
 
 
