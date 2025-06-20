d7e33962e96a8113ec549069fd881b2121abce8e
towerinwater <haitri62@gmail.com>
2025-06-20 09:56:37 +0000

    [automatic commit] 2025-06-20 09:56:37 UTC 129.127.202.120

--- part2/routes/owner.js
+++ part2/routes/owner.js
@@ -3,8 +3,12 @@ const router = express.Router();
 const db = require('../models/db');
 
 router.post('/show_dogs', (req, res, next) => {
-    if(req.session.user)
-    const uid = req.session.user.user_id;
+    if(req.session.user){
+        const uid = req.session.user.user_id;
+    }
+    else{
+        return res.status(404).
+    }
 
     try{
         const[rows] = db.query(
