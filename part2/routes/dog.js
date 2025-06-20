3db4d18db3a8275ef99ce6b8913227cb66c24217
towerinwater <haitri62@gmail.com>
2025-06-20 11:36:22 +0000

    [automatic commit] 2025-06-20 11:36:22 UTC 129.127.202.120

--- part2/routes/dog.js
+++ part2/routes/dog.js
@@ -2,6 +2,23 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models/db');
 
-
+router.get('/dogs', async (req, res) => {
+    try{
+        const [rows] = await db.query(`
+            SELECT
+                d.name AS dog_name,
+                d.size AS size,
+                u.username AS owner_username
+            FROM Dogs AS d
+            INNER JOIN Users AS u ON
+                d.owner_id = u.user_id
+        `);
+        res.json(rows);
+    }
+    catch(err){
+        console.log(err);
+        res.status(500).json({ error: 'Failed to fetch database.' });
+    }
+});
 
 module.exports = router;
