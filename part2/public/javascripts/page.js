d7a6267d2d2401821ee83ef9f5384d15965ff358
towerinwater <haitri62@gmail.com>
2025-06-20 06:19:40 +0000

    [automatic commit] 2025-06-20 06:19:40 UTC 129.127.202.120

--- part2/public/javascripts/page.js
+++ part2/public/javascripts/page.js
@@ -8,7 +8,8 @@ createApp({
                 password: ''
             },
             CurrUser: {
-                username: '',
+                name: '',
+                role: '',
                 
             }
         };
