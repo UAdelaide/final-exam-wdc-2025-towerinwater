678192ac1c902df7a5386368beb84e22b78b5467
towerinwater <haitri62@gmail.com>
2025-06-20 06:28:04 +0000

    [automatic commit] 2025-06-20 06:28:04 UTC 129.127.202.120

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -49,7 +49,7 @@ router.post('/login', async (req, res) => {
 			return res.status(401).json({ error: 'Invalid credentials' });
 		}
 
-		req.session.user = {}
+		req.session.user = {username: rows.username, user_id, }
 		res.json({ message: 'Login successful', user: rows[0] });
 	} catch (error) {
 		res.status(500).json({ error: 'Login failed' });
