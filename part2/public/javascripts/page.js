42197895d0946d473219b31b9057124fd14757ce
towerinwater <haitri62@gmail.com>
2025-06-20 06:16:38 +0000

    [automatic commit] 2025-06-20 06:16:38 UTC 129.127.202.120

--- part2/public/javascripts/page.js
+++ part2/public/javascripts/page.js
@@ -12,12 +12,12 @@ createApp({
     methods: {
         async login(){
             try{
-                const res = await fetch('users/login', {
+                await fetch('users/login', {
                     method: 'POST',
                     headers: { 'Content-Type': 'application/json'},
                     body: JSON.stringify({user})
-                });
-                    
+                })
+                .then()
             }
         }
     }
