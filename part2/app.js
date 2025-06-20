1faa93ceb71a83dac7cd5ef2440cb55a9d0cae51
towerinwater <haitri62@gmail.com>
2025-06-20 06:36:49 +0000

    [automatic commit] 2025-06-20 06:36:49 UTC 129.127.202.120

--- part2/app.js
+++ part2/app.js
@@ -30,8 +30,8 @@ app.use(express.static(path.join(__dirname, '/public')));
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
 
-app.use('/api/walks', walkRoutes);
-app.use('/api/users', userRoutes);
+app.use('/walks', walkRoutes);
+app.use('/users', userRoutes);
 
 // Export the app instead of listening here
 module.exports = app;
