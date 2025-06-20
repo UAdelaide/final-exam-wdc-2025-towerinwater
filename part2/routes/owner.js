44d2ec8f22074cc80500402feeaf2377e3d56523
towerinwater <haitri62@gmail.com>
2025-06-20 09:57:37 +0000

    [automatic commit] 2025-06-20 09:57:37 UTC 129.127.202.120

--- part2/routes/owner.js
+++ part2/routes/owner.js
@@ -7,7 +7,7 @@ router.post('/show_dogs', (req, res, next) => {
         const uid = req.session.user.user_id;
     }
     else{
-        return res.status(400).json({message: ''})
+        return res.status(400).json({message: 'Access Period Expired.'})
     }
 
     try{
