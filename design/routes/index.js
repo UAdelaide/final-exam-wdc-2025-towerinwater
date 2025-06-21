8c2a9c5f79ae7f16fcfeea0156b6f3f63711ee96
towerinwater <haitri62@gmail.com>
2025-06-21 03:19:10 +0000

    [automatic commit] 2025-06-21 03:19:10 UTC 129.127.202.120

--- design/routes/index.js
+++ design/routes/index.js
@@ -4,31 +4,31 @@ var router = express.Router();
 /* Taken from the models of part 2 */
 const mysql = require('mysql2/promise');
 const db = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  database: 'DogWalkService',
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+	host: 'localhost',
+	user: 'root',
+	database: 'DogWalkService',
+	waitForConnections: true,
+	connectionLimit: 10,
+	queueLimit: 0
 });
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+	res.render('index', { title: 'Express' });
 });
 
 /* From part 2 dog router */
 router.get('/show', async (req, res) => {
-    try{
-        const [rows] = await db.query(`
+	try {
+		const [rows] = await db.query(`
             SELECT * FROM Dogs
         `);
-        res.status(200).json({ message: 'Successfully Loaded Info.', info: rows });
-    }
-    catch(err){
-        console.log(err);
-        res.status(500).json({ error: 'Internal Errors.' });
-    }
+		res.status(200).json({ message: 'Successfully Loaded Info.', info: rows });
+	}
+	catch (err) {
+		console.log(err);
+		res.status(500).json({ error: 'Internal Errors.' });
+	}
 });
 
 module.exports = router;
