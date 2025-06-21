7fff8e35446b176f45ddd822d2743246842e0a55
towerinwater <haitri62@gmail.com>
2025-06-21 03:20:40 +0000

    [automatic commit] 2025-06-21 03:20:40 UTC 129.127.202.120

---
+++ design/public/javascripts/index.js
@@ -0,0 +1,331 @@
+const { createApp } = Vue;
+createApp({
+    data() {
+        return {
+            message: 'Welcome to the Dog Walking Service!',
+            /*Storing the login info of the user*/
+            UserLogin: {
+                username: '',
+                password: ''
+            },
+            /*Store the data of the current user*/
+            CurrUser: {
+                uid: null,
+                name: '',
+                role: '',
+                email: ''
+            },
+            /* Store all the data of the dog */
+            dogs: [],
+            /* Default image when the dogs' images are not found */
+            img_placeholder: 'images/logo.png'
+        };
+    },
+
+    async mounted(){
+        await fetch('/dog/show')
+        .then((res) => {
+            /* Successfully got the result and parse the json for result or log message */
+            res.json().then((info) => {
+                if(!res.ok){
+                    alert(info.error);
+                    throw new Error(info.error);
+                }
+                this.dogs = info.info;
+
+                /*Putting the image for each of the dog*/
+                this.dogs.forEach(async (dog) => {
+                    try{
+                        await fetch('https://dog.ceo/api/breeds/image/random')
+                        .then((res) => {
+                            res.json().then((info) => {
+                                if(!res.ok){
+                                    alert(info.status);
+                                    throw new Error('Can not fetch image');
+                                }
+                                dog.imgURL = info.message;
+                                console.log(dog);
+                                /* Log message make sure everything is working */
+                                // alert(info.status);
+                            });
+                        });
+                    }
+                    catch{
+                        dog.imgURL = img_placeholder;
+                    }
+                });
+                /* Log message make sure everything is working */
+                alert(info.message);
+            });
+        });
+
+    },
+
+    methods: {
+        //login function which will be prompted by the 'button' from the html when submit, using the Vue framework.
+        async login(){
+            //initialise the data which will be sent.
+            const user = {
+                username: this.UserLogin.username,
+                password: this.UserLogin.password
+            };
+
+            await fetch('users/login', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json'},
+                body: JSON.stringify(user)
+            })
+            .then((res) => {    //Having got the response
+                res.json().then((info) => {
+                    //Check if the credential is aligned.
+                    if(res.status === 401){
+                        alert(info.error);
+                        throw new Error(info.error);
+                    }
+                    //other errors which are not related to the credential but mostly server side.
+                    else if(!res.ok){
+                        alert(info.error)
+                        throw new Error(info.error);
+                    }
+
+                    /*Whenever there is error, throw new Error(...) will break out of the chain.*/
+                    /*At the end, the data has been retrieved correctly and assigned to the current user.*/
+                    /*Then take them to the corresponding page of their roles.*/
+                    this.CurrUser.uid = info.user.user_id;
+                    this.CurrUser.name = info.user.username;
+                    this.CurrUser.role = info.user.role;
+                    this.CurrUser.email = info.user.email;
+
+                    /*Taking them to their page*/
+                    const role = this.CurrUser.role;
+                    if(role === 'owner') window.location.href = '/owner-dashboard.html';
+                    else if(role === 'walker') window.location.href = '/walker-dashboard.html';
+
+                    alert(info.message);
+                });
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+        }
+    }
+}).mount('#app');
+
+
+
+var posts = [];
+var search = null;
+
+/*
+ * Hides the main part of the page to show the Ask a Question section
+ */
+function showAsk(){
+    var main = document.getElementById("main");
+    var ask = document.getElementById("ask");
+    main.style.display = "none";
+    ask.style.display = "block";
+}
+
+/*
+ * Hides the Ask a Question section of the page to show the main part,
+ * clearing the question input fields.
+ */
+function showMain(){
+    var main = document.getElementById("main");
+    var ask = document.getElementById("ask");
+    ask.style.display = "none";
+    main.style.display = "block";
+
+    document.getElementById('post-title').value = '';
+    document.getElementById('post-content').value = '';
+    document.getElementById('post-tags').value = '';
+}
+
+/*
+ * Creates a new question/post & send it to the server, before triggering an update for the main part of the page.
+ */
+function createPost(){
+
+    search = null;
+
+    let post = {
+        title: document.getElementById('post-title').value,
+        content: document.getElementById('post-content').value,
+        tags: document.getElementById('post-tags').value.split(" "),
+        upvotes: 0
+    };
+
+    // Create AJAX Request
+    var xmlhttp = new XMLHttpRequest();
+
+    // Define function to run on response
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            // Update the page on success
+            loadPosts();
+            showMain();
+        }
+    };
+
+    // Open connection to server & send the post data using a POST request
+    // We will cover POST requests in more detail in week 8
+    xmlhttp.open("POST", "/addpost", true);
+    xmlhttp.setRequestHeader("Content-type", "application/json");
+    xmlhttp.send(JSON.stringify(post));
+
+}
+
+/*
+ * Updates the search term then reloads the posts shown
+ */
+function searchPosts(){
+
+    search = document.getElementById('post-search').value.toUpperCase();
+    updatePosts();
+
+}
+
+
+/*
+ * Reloads the posts shown on the page
+ * Iterates over the array of post objects, rendering HTML for each and appending it to the page
+ * If a search term is being used
+ */
+function updatePosts() {
+
+    // Reset the page
+    document.getElementById('post-list').innerHTML = '';
+
+    // Iterate over each post in the array by index
+    for(let i=0; i<posts.length; i++){
+
+        let post = posts[i];
+
+        // Check if a search term used.
+        if(search !== null){
+            // If so, skip this question/post if title or content doesn't match
+            if (post.title.toUpperCase().indexOf(search) < 0 &&
+                post.content.toUpperCase().indexOf(search) < 0 ) {
+                continue;
+            }
+        }
+
+        // Generate a set of spans for each of the tags
+        let tagSpans = '';
+        for(let tag of post.tags){
+            tagSpans = tagSpans + `<span class="tag">${tag}</span>`;
+        }
+
+        // Generate the post/question element and populate its inner HTML
+        let postDiv = document.createElement("DIV");
+        postDiv.classList.add("post");
+
+        postDiv.innerHTML = `
+            <div class="votes">
+                <button onclick="upvote(${i})">+</button>
+                <p><span class="count">${post.upvotes}</span><br />votes</p>
+                <button onclick="downvote(${i})">-</button>
+            </div>
+            <div class="content">
+                <h3><a href="#">${post.title}</a></h3>
+                <i>By ${post.author}</i>
+                <p>${post.content}</p>
+                ${tagSpans}<span class="date">${new Date(post.timestamp).toLocaleString()}</span>
+            </div>
+        `;
+
+        // Append the question/post to the page
+        document.getElementById("post-list").appendChild(postDiv);
+
+    }
+
+
+}
+
+/*
+ * Loads posts from the server
+ * - Send an AJAX GET request to the server
+ * - JSON Array of posts sent in response
+ * - Update the
+ */
+function loadPosts() {
+
+    // Create AJAX Request
+    var xmlhttp = new XMLHttpRequest();
+
+    // Define function to run on response
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            // Parse the JSON and update the posts array
+            posts = JSON.parse(this.responseText);
+            // Call the updatePosts function to update the page
+            updatePosts();
+        }
+    };
+
+    // Open connection to server
+    xmlhttp.open("GET", "/posts", true);
+
+    // Send request
+    xmlhttp.send();
+
+}
+
+
+/*
+ * Increase the votes for a given post, then update the page
+ */
+function upvote(index) {
+    posts[index].upvotes ++;
+    updatePosts();
+}
+
+/*
+ * Decrease the votes for a given post, then update the page
+ */
+function downvote(index) {
+    posts[index].upvotes --;
+    updatePosts();
+}
+
+
+/*I commented the AJAX out, because I want to use everything accordingly with the Vue framework.*/
+
+/*
+function login(){
+
+    let user = {
+        user: document.getElementById('username').value,
+        pass: document.getElementById('password').value
+    };
+
+    // Create AJAX Request
+    var xmlhttp = new XMLHttpRequest();
+
+    // Define function to run on response
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            alert("Welcome "+ this.responseText);
+        } else if (this.readyState == 4 && this.status >= 400) {
+            alert("Login failed");
+        }
+    };
+
+    // Open connection to server & send the post data using a POST request
+    // We will cover POST requests in more detail in week 8
+    xmlhttp.open("POST", "/users/login", true);
+    xmlhttp.setRequestHeader("Content-type", "application/json");
+    xmlhttp.send(JSON.stringify(user));
+
+}
+
+function logout(){
+    // Create AJAX Request
+    var xmlhttp = new XMLHttpRequest();
+
+    // Open connection to server & send the post data using a POST request
+    xmlhttp.open("POST", "/users/logout", true);
+    xmlhttp.send();
+
+}
+*/
