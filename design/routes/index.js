3c44991ef87b08ea30380cc254950d1a79216c4d
towerinwater <haitri62@gmail.com>
2025-06-21 03:17:19 +0000

    [automatic commit] 2025-06-21 03:17:19 UTC 129.127.202.120

--- design/routes/index.js
+++ design/routes/index.js
@@ -6,6 +6,17 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-
+router.get('/show', async (req, res) => {
+    try{
+        const [rows] = await db.query(`
+            SELECT * FROM Dogs
+        `);
+        res.status(200).json({ message: 'Successfully Loaded Info.', info: rows });
+    }
+    catch(err){
+        console.log(err);
+        res.status(500).json({ error: 'Internal Errors.' });
+    }
+});
 
 module.exports = router;
