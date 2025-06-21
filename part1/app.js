3fc2898b66a0f9a1cae063d9a9c2f8749e1666e6
towerinwater <haitri62@gmail.com>
2025-06-21 02:52:35 +0000

    [automatic commit] 2025-06-21 02:52:35 UTC 129.127.202.120

--- part1/app.js
+++ part1/app.js
@@ -14,9 +14,8 @@ app.use(cookieParser());
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-
 app.use('/', indexRouter);
-app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 let db;
 
