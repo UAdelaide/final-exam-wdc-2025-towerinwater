a529d1a72ef90d4602b6330f43a4a65e7aca3d2d
towerinwater <haitri62@gmail.com>
2025-06-21 03:04:22 +0000

    [automatic commit] 2025-06-21 03:04:22 UTC 129.127.202.120

--- part1/app.js
+++ part1/app.js
@@ -23,10 +23,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
 
 /* Start of the part 1 section */
 /* Initialise the database */
@@ -153,6 +149,10 @@ app.get('/api/walkers/summary', async (req, res) => {
 /* End of those api/* calls */
 /* End of  the part 1 */
 
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
 
 // error handler
 app.use(function(err, req, res, next) {
